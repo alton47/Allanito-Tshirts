84fd81940107b370a9dca7208a723514ae68bbbc
Allan <altonallan47@gmail.com>
2023-03-31 22:29:45 +0200

    Update Index.Canvas

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center } from '@react-three/drei'
 
-const Canvas = () => {
+import Shirt from './Shirt'
+import CameraRig from './CameraRig'
+import Backdrop from './Backdrop'
+
+
+const CanvasModel = () => {
   return (
-    <div>Canvas</div>
+    <Canvas>
+        <ambientLight intensity = {0.5} />
+        <Environment preset = 'city' />
+    </Canvas>
   )
 }
 
