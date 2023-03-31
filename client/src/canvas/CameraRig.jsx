89f7d4b6165e75112f25e1379cc4af1225518ad6
Allan <altonallan47@gmail.com>
2023-03-31 22:55:38 +0200

    Update CameraRig

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+import state from '../store';
 
-const CameraRig = () => {
-  return (
-    <div>CameraRig</div>
-  )
+
+
+const CameraRig = ({ children }) => {
+    const group = useRef();
+
+
+  return <group ref = {group}>
+    {children}
+  </group>
 }
 
 export default CameraRig
