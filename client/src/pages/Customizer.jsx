7ebe4556a443c0e4cfed11815412843fc189bfdf
Allan <altonallan47@gmail.com>
2023-03-31 22:09:39 +0200

    Update Customizer

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import { AnimatePresence, motion } from 'framer-motion'
-import { useSnapshot } from 'valtio'
+import React, { useState, useEffect } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
 import config from '../config/config';
 import state from '../store';
 import { download } from '../assets';
@@ -12,22 +12,22 @@ import { ColorPicker, AIPicker, Tab, CustomButton, FilePicker } from '../compone
 const Customizer = () => {
     const snap = useSnapshot(state);
 
-
   return (  
     <AnimatePresence>
 
         {!snap.intro && (
             <>
             <motion.div
-            key = 'custom'
-            className='absolute top-0 left-0 z-10'
-            {...slideAnimation('left')}>
+              key = 'custom'
+              className = 'absolute top-0 left-0 z-10'
+              {...slideAnimation('left')}>
                 <div className='flex items-center min-h-screen'>
                     <div className='editortabs-container tabs'>
-                        {EditorTabs-Map((tab) => (
+                        {EditorTabs.map((tab) => (
                             <Tab 
                             key = {tab.name}
-                            tab = {tab}/>
+                            tab = {tab}
+                            handleClick = {() => {}}/>
                         ))}
                     </div>
                 </div>
@@ -35,10 +35,24 @@ const Customizer = () => {
 
 
             <motion.div
-            key = 'custom'
             className='absolute top-5 right-5 z-10'
-            {...fadeAnimation('left')}>
-                
+            {...fadeAnimation}>
+                <CustomButton 
+                   type = 'filled'
+                   title = 'Go Back'
+                   handleClick={() => state.intro = true}
+                   customStyles = 'w-fit px-4 py-2.5 font-bold text-sm'/>          
+            </motion.div>
+
+
+             <motion.div
+            className='filtertabs-container'
+            {...slideAnimation('up')}>
+                <Tab 
+                   type = 'filled'
+                   title = 'Go Back'
+                   handleClick={() => state.intro = true}
+                   customStyles = 'w-fit px-4 py-2.5 font-bold text-sm'/>          
             </motion.div>
             </>
         )}
