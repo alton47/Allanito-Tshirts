829c974c8dc81a766a2a83f77312ddc0bbb26038
Allan <altonallan47@gmail.com>
2023-04-01 10:28:55 +0200

    Update CameraRig

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -8,7 +8,35 @@ import state from '../store';
 
 const CameraRig = ({ children }) => {
     const group = useRef();
+    const snap = useSnapshot(state);
 
+    useFrame((state, delta) => {
+        const isBreakpoint = window.innerWidth <= 1260;
+        const isMobile = window.innerWidth <= 600;
+
+
+        //Initial Position of Tshirt
+        let targetPosition = [-0.4, 0, 2];
+        if(snap.intro){
+            if(isBreakpoint) targetPosition = [0, 0, 2];
+            if(isMobile) targetPosition = [0, 0.2, 2.5];
+        }else {
+            if(isMobile) targetPosition = [0, 0, 2.5]
+            else targetPosition = [0, 0, 2];
+        }
+
+ 
+        //Set T-shirt rotationSmoothly
+        easing.dampE(
+            group.current.rotation,
+            [state.pointer.y / 10, -state.pointer.x / 5, 0],
+            0.25,
+            delta
+        )
+    
+    })
+
+    
 
   return <group ref = {group}>
     {children}
