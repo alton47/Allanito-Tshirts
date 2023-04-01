3a3fe2a1761de85f0088a5a830fa616685bf3fcc
Allan <altonallan47@gmail.com>
2023-04-01 13:34:12 +0200

    Update Home.js

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,9 +25,9 @@ const Home = () => {
             <motion.section className='home' {...slideAnimation('left')}>
                 <motion.header {...slideAnimation('down')}>
                     <img 
-                    src="./threejs.png" 
+                    src="./FavIcon.svg" 
                     alt="logo"
-                    className='w-8 h-8 object-contain' />
+                    className='w-8 h-8 object-contain' /> 
                 </motion.header>
 
 
